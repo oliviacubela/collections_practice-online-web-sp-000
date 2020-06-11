8a9189b526382bc259a2763025f04fb4e785cfb3
Olivia Cubela <oliviacubela@gmail.com>
2020-06-11 20:22:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,29 +4,29 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort do |a, b|
+  array.sort do |b, a|
     b <=> a
 end
 
-def sort_array_char_count
-
-end
-
-def swap_elements
-
-end
-
-def reverse_array
-end
-
-def kesha_maker
-end
-
-def find_a
-end
-
-def sum_array
-end
-
-def add_s
-end
+# def sort_array_char_count
+# 
+# end
+# 
+# def swap_elements
+# 
+# end
+# 
+# def reverse_array
+# end
+# 
+# def kesha_maker
+# end
+# 
+# def find_a
+# end
+# 
+# def sum_array
+# end
+# 
+# def add_s
+# end
