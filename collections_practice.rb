1e048c23a109e59bd2bb0ff9aafc4455e8e26da7
Olivia Cubela <oliviacubela@gmail.com>
2020-06-11 20:20:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  a <=> b
+end
 
+end
+
+def sort_array_desc
+
+end
+
+def sort_array_char_count
+
+end
+
+def swap_elements
+
+end
+
+def reverse_array
+end
+
+def kesha_maker
+end
+
+def find_a
+end
+
+def sum_array
+end
+
+def add_s
+end
